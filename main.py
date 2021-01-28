55a458bf1ab8af31f822d4ee36ab80f756ba4f89
Nosek Kamil <Kamil.Nosek@t-mobile.pl>
2021-01-28 13:00:39 +0100

    small update of project

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-from flask import Flask, request, Response, render_template
+from flask import Flask, request, Response, render_template, jsonify
 import json
 import mysql.connector
-# from flask_cors import CORS
-
-# LET's CREATE A REST API FOR NEWSLETTER
-# it needs 2 endpoints
-# One GET endpoint ('/showEmails/) that will show all the emails saved on the server in JSON format (response: JSON)
-# One POST endpoint ('/addEmail/) that will receive a `name` and an `email` in the Request body as JSON (request: JSON)
-# it will also return 'True' or 'False' wether you saved it successfully or not (response: STRING)
-# save all new emails in a file, do not replace the old ones
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -37,27 +29,28 @@ def showEmails():
     querySelect = ("SELECT * "
                    "FROM subscribers")
     cursor.execute(querySelect)
-    return json.dumps(cursor.fetchall(), indent=4)
+
+    json_data = []
+    json_tuples = cursor.fetchall()
+
+    for obj in json_tuples:
+        json_data.append(dict({"id": obj[0], "name": obj[1], "email": obj[2]}))
+
+    return json.dumps(json_data)
 
 @app.route('/addEmail', methods=['POST'])
 def addEmail():
     try:
-        with open('emails.json') as json_file:
-            json_data = json.load(json_file)
+        if request.is_json and request.json.get('name') and request.json.get('email'):
 
-            last_id = json_data[-1]['id']
+            name = request.json.get('name')
+            email = request.json.get('email')
 
-            post_data = request.get_json()
-            post_data['id'] = last_id + 1
+            query = (' INSERT INTO subscribers VALUES(NULL, %s, %s)')
 
-            json_data.append(post_data)
+            cursor.execute(query, (name, email))
+            return "True"
 
-            json_updated = json.dumps(json_data, indent=4)
-
-        file = open('emails.json', "w")
-        file.write(json_updated)
-
-        return json_updated
     except:
 
         return "Something went wrong", 500
--- static/getJSON.js
+++ static/getJSON.js
@@ -4,8 +4,6 @@ window.addEventListener('DOMContentLoaded', (e) => {
     const tableBody = document.querySelector('.table-body')
     let allIdElements = document.querySelectorAll('.id');
 
-    console.log(allIdElements)
-
     button.addEventListener('click', (e) => {
 
         fetch('http://localhost:5000/showEmails')
@@ -13,83 +11,59 @@ window.addEventListener('DOMContentLoaded', (e) => {
                 return response.json()
             })
             .then((data) => {
-                console.log(data)
-                createAndAddElements(data)
+                updateTable(data)
             })
             .catch((err) => {
                 console.error("error:", err)
             })
-
     })
 
-//    const updateTable = (data) => {
-//        if (allIdElements.length < 1) {
-//            createAndAddElements(data)
-//        } else {
-//            for (let i = 0; i < allIdElements.length; i++) {
-//                if (allIdElements.innerText ===)
-//            }
-//        }
-//    }
-
-    const createAndAddElements = (data) => {
-        let jsonArr = JSON.parse(data)
-
-        if (allIdElements.length < 1 ) {
-            console.log("abc more123")
-
+    const updateTable = (data) => {
+        let jsonArr = data
+        if (allIdElements.length < 1) {
             for (let i = 0; i < jsonArr.length; i++ ) {
-                let row = document.createElement("tr")
-                row.classList.add('active-row')
-
-                let tdId = document.createElement("td")
-                tdId.classList.add('id')
-                let tdName = document.createElement("td")
-                let tdEmail = document.createElement("td")
-
-                let idTxt = document.createTextNode(jsonArr[i].id)
-                let nameTxt = document.createTextNode(jsonArr[i].name)
-                let emailTxt = document.createTextNode(jsonArr[i].email)
-
-                tdId.appendChild(idTxt)
-                tdName.appendChild(nameTxt)
-                tdEmail.appendChild(emailTxt)
-
-                row.appendChild(tdId);
-                row.appendChild(tdName);
-                row.appendChild(tdEmail);
-
-                tableBody.appendChild(row)
+                createAndAdd(data, i)
             }
-
             allIdElements = document.querySelectorAll('.id');
+
+        } else if (jsonArr.length < allIdElements.length) {
+            tableBody.innerHTML = ""
+            for (let i = 0; i < jsonArr.length; i++ ) {
+                createAndAdd(data, i)
+            }
         } else {
             for (let i = 0; i < jsonArr.length; i++ ) {
-
                 if (allIdElements[i] == null) {
-                    let row = document.createElement("tr")
-                    row.classList.add('active-row')
-
-                    let tdId = document.createElement("td")
-                    tdId.classList.add('id')
-                    let tdName = document.createElement("td")
-                    let tdEmail = document.createElement("td")
-
-                    let idTxt = document.createTextNode(jsonArr[i].id)
-                    let nameTxt = document.createTextNode(jsonArr[i].name)
-                    let emailTxt = document.createTextNode(jsonArr[i].email)
-
-                    tdId.appendChild(idTxt)
-                    tdName.appendChild(nameTxt)
-                    tdEmail.appendChild(emailTxt)
-
-                    row.appendChild(tdId);
-                    row.appendChild(tdName);
-                    row.appendChild(tdEmail);
-
-                    tableBody.appendChild(row)
+                    createAndAdd(data, i)
+                    allIdElements = document.querySelectorAll('.id');
                 }
             }
         }
+    }
+
+    const createAndAdd = (data, i) => {
+        let jsonArr = data;
+
+        let row = document.createElement("tr")
+        row.classList.add('active-row')
+
+        let tdId = document.createElement("td")
+        tdId.classList.add('id')
+        let tdName = document.createElement("td")
+        let tdEmail = document.createElement("td")
+
+        let idTxt = document.createTextNode(Object.assign({}, jsonArr[i]).id)
+        let nameTxt = document.createTextNode(Object.assign({}, jsonArr[i]).name)
+        let emailTxt = document.createTextNode(Object.assign({}, jsonArr[i]).email)
+
+        tdId.appendChild(idTxt)
+        tdName.appendChild(nameTxt)
+        tdEmail.appendChild(emailTxt)
+
+        row.appendChild(tdId);
+        row.appendChild(tdName);
+        row.appendChild(tdEmail);
+
+        tableBody.appendChild(row)
     }
 })
